f8a4f6f5d07a2b6011cb9ca25c242b233b33f8d2
Rich Gerdes <richard.gerdes@rutgers.edu>
2016-08-08 16:26:05 -0400

    initial role storage override for inheritance

--- src/RoleInheritanceStorage.php
+++ src/RoleInheritanceStorage.php
@@ -11,4 +11,15 @@ use \Drupal\user\RoleStorage;
 
 class RoleInheritanceStorage extends RoleStorage{
     
+    public function isPermissionInRoles($permission, array $rids) {
+        
+        $extendedRids = _role_inheritance_extendroles($rids);
+        
+        $p = parent::isPermissionInRoles($permission, $extendedRids);
+        
+        error_log(var_export(array($permissionk, $p), true));
+        
+        return $p;
+    }
+    
 }
