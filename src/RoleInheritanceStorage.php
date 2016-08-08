9f2d09438cac4fb48fad93d186862020a7b6b606
Rich Gerdes <richard.gerdes@rutgers.edu>
2016-08-08 11:42:35 -0400

    Initila Source

---
+++ src/RoleInheritanceStorage.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\user\RoleStorage.
+ */
+
+namespace Drupal\role_inheritance;
+
+use \Drupal\user\RoleStorage;
+
+class RoleInheritanceStorage extends RoleStorage{
+    
+}
