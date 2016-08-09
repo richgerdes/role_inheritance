0d44fb547b2d271f40933f2cc38b10a6750ce58b
Rich Gerdes <richard.gerdes@rutgers.edu>
2016-08-09 14:57:49 -0400

    Formatting

--- src/RoleInheritanceStorage.php
+++ src/RoleInheritanceStorage.php
@@ -10,16 +10,16 @@ namespace Drupal\role_inheritance;
 use \Drupal\user\RoleStorage;
 
 class RoleInheritanceStorage extends RoleStorage{
-    
-    public function isPermissionInRoles($permission, array $rids) {
-        
-        $extendedRids = _role_inheritance_extendroles($rids);
-        
-        $p = parent::isPermissionInRoles($permission, $extendedRids);
-        
-        error_log(var_export(array($permissionk, $p), true));
-        
-        return $p;
-    }
-    
+
+  public function isPermissionInRoles($permission, array $rids) {
+
+    $extendedRids = _role_inheritance_extendroles($rids);
+
+    $p = parent::isPermissionInRoles($permission, $extendedRids);
+
+    error_log(var_export(array($permissionk, $p), true));
+
+    return $p;
+  }
+
 }
