706f23d8a20bfd43b768144aefde1b01fa01ed31
Rich Gerdes <richard.gerdes@rutgers.edu>
2016-10-11 13:44:14 -0400

    rebuild logic to mimic changes to content provders/role access module

--- src/Form/RoleInheritanceAdminForm.php
+++ src/Form/RoleInheritanceAdminForm.php
@@ -25,19 +25,9 @@ class RoleInheritanceAdminForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $role_names = array();
-    foreach (user_roles() as $role_name => $role) {
-      // Retrieve role names for columns.
-      $role_names[$role_name] = $role->label();
-    }
+    $roles = user_roles();
 
     $role_mapping = _role_inheritance_roleMap();
-
-    // Store $role_names for use when saving the data.
-    $form['role_names'] = array(
-      '#type' => 'value',
-      '#value' => $role_names,
-    );
 
     $form['inheritance'] = array(
       '#type' => 'table',
@@ -46,28 +36,24 @@ class RoleInheritanceAdminForm extends FormBase {
       '#attributes' => ['class' => ['role-inheritance-all', 'js-role-inheritance-all']],
       '#sticky' => TRUE,
     );
-    
-    foreach ($role_names as $name) {
+
+    foreach ($roles as $rid => $role) {
       $form['inheritance']['#header'][] = array(
-        'data' => $name,
+        'data' => $role->label(),
         'class' => array('checkbox'),
       );
     }
 
-    foreach ($role_names as $rid => $name) {
+    foreach ($roles as $rid => $role) {
       // Fill in default values for the permission.
-      $perm_item = array(
-        'title' => $name,
-        'restrict access' => FALSE,
-      );
       $form['inheritance'][$rid]['description'] = array(
         '#type' => 'inline_template',
         '#template' => '<div class="permission"><span class="title">{{ title }}</span></div>',
         '#context' => array(
-          'title' => $name,
+          'title' => $role->label(),
         ),
       );
-      foreach ($role_names as $srid => $sname) {
+      foreach ($roles as $srid => $srole) {
         if($srid == $rid){
           $form['inheritance'][$rid][$srid] = array(
             '#type' => 'inline_template',
@@ -76,7 +62,7 @@ class RoleInheritanceAdminForm extends FormBase {
           );
         }else{
           $form['inheritance'][$rid][$srid] = array(
-            '#title' => $sname . ': ' . $perm_item['title'],
+            '#title' => $role->label() . ' inherits from ' .  $srole->label(),
             '#title_display' => 'invisible',
             '#wrapper_attributes' => array(
               'class' => array('checkbox'),
@@ -93,7 +79,7 @@ class RoleInheritanceAdminForm extends FormBase {
         }
       }
     }
-    
+
 
     $form['actions'] = array('#type' => 'actions');
     $form['actions']['submit'] = array(
@@ -118,7 +104,7 @@ class RoleInheritanceAdminForm extends FormBase {
 
     $values = $form_state->getValues();
     $roles = array_keys(user_roles());
-    
+
     $mapping = array();
 
     foreach ($roles as $role){
@@ -129,9 +115,9 @@ class RoleInheritanceAdminForm extends FormBase {
         }
       }
     }
-    
+
     _role_inheritance_roleMap($mapping);
-      
+
     drupal_flush_all_caches();
   }
 }
